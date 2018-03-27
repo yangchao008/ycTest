971cf667c9fb4e1a92bbcb2209d7fdddd69ade12
29467 <18770090820@163.com>
2018-03-27 10:29:30 +0800

    hht update

--- app/src/main/java/com/vnetoo/test/activity/XImageViewActivity.java
+++ app/src/main/java/com/vnetoo/test/activity/XImageViewActivity.java
@@ -11,6 +11,9 @@ import android.view.MotionEvent;
 import com.baidu.platform.comapi.map.B;
 import com.bm.library.PhotoView;
 import com.vnetoo.test.R;
+import com.vnetoo.test.utils.MyLog;
+
+import java.util.Queue;
 
 import cn.kejin.ximageview.XImageView;
 
@@ -20,7 +23,9 @@ import cn.kejin.ximageview.XImageView;
  * Comment: //TODO
  */
 public class XImageViewActivity extends FragmentActivity{
+    final String TAG = getClass().getName();
     XImageView mXImageView;
+    StringBuffer msg = new StringBuffer();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,5 +60,72 @@ public class XImageViewActivity extends FragmentActivity{
 
             }
         });
+        TreeNode a = new TreeNode();
+        a.value = "A";
+        TreeNode b = new TreeNode();
+        b.value = "B";
+        TreeNode c = new TreeNode();
+        c.value = "C";
+        TreeNode d = new TreeNode();
+        d.value = "D";
+        TreeNode e = new TreeNode();
+        e.value = "E";
+        TreeNode f = new TreeNode();
+        f.value = "F";
+        TreeNode g = new TreeNode();
+        g.value = "G";
+
+        a.left = b;
+        a.right = c;
+
+        b.left = d;
+        b.right = e;
+
+        c.left = f;
+        c.right = g;
+
+        printPreTree(a);
+        MyLog.l(TAG,"printPreTree = " + msg);
+        msg = new StringBuffer();
+        printMidTree(a);
+        MyLog.l(TAG,"printMidTree = " + msg);
+        msg = new StringBuffer();
+        printAfterTree(a);
+        MyLog.l(TAG,"printAfterTree = " + msg);
+    }
+
+    public void printPreTree(TreeNode root){
+        if (root == null)
+            return;
+        msg.append( "--" + root.value);
+        printPreTree(root.left);
+        printPreTree(root.right);
+
+    }
+
+    public void printMidTree(TreeNode root){
+        if (root == null)
+            return;
+        printPreTree(root.left);
+        msg.append( "--" + root.value);
+        printPreTree(root.right);
+
+    }
+
+    public void printAfterTree(TreeNode root){
+        if (root == null)
+            return;
+        printPreTree(root.left);
+        printPreTree(root.right);
+        msg.append( "--" + root.value);
+    }
+
+    /**
+     ** 二叉树节点
+     **/
+    public class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        String value;
     }
 }
